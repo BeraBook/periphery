a640f18c2e94da8633c084c4bc6a0e31672aa400
Sean <detectivekim1@gmail.com>
2024-11-26 22:42:00 +0900

    feat: deploy on mitosis

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,7 +3,7 @@ import { Address } from 'viem'
 
 export const BOOK_MANAGER: { [chainId: number]: Address } = {
   [berachainTestnetbArtio.id]: '0x0Cd2C356be90864F4a5e0551E79dd039b246FaCA',
-  [124832]: '0x9C6C405cbB2c1DC7aAAa65156744fC00efc7EC82',
+  [124832]: '0x874b1B795993653fbFC3f1c1fc0469214cC9F4A5',
 }
 
 export const SAFE_WALLET: { [chainId: number]: Address } = {}
